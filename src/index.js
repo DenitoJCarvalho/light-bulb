cb5781b9665fc2fc118ab7722c7dcdb2bd88bfad
Dennis J de Carvalho <dennisjcaralho@gmail.com>
2023-03-17 11:37:09 -0300

    update: retirado comentarios desnecessarios

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
-
-// const btnAcender = document.querySelector("#acender")
-// const btnApagar = document.querySelector("#apagar")
-
-// //acender luz
-// btnAcender.addEventListener('click', () => {
-//     let imgAcender = document.querySelector("#acenderLuz")
-//     let imgApagar = document.querySelector("#apagarLuz")
-
-//     imgApagar.style.display = "none"
-//     setTimeout(() => {
-//         imgApagar.style.display = "none"
-//         imgAcender.style.display = "flex"
-//     }, 120)
-
-// })
-
-// //apagar luz
-// btnApagar.addEventListener('click', () => {
-//     let imgAcender = document.querySelector("#acenderLuz")
-//     let imgApagar = document.querySelector("#apagarLuz")
-
-//     setTimeout(() => {
-//         imgAcender.style.display = 'none'
-//         imgApagar.style.display = 'flex'
-//     }, 120)
-// })
 "strict mode"
 
 let turnOff = document.querySelector('button#apagar');
